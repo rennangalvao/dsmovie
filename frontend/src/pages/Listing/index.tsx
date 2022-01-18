2cc2079db7990c8260c015458840410e1dd273d4
rennangalvao <inc.rennan@gmail.com>
2022-01-17 21:53:10 -0500

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,17 @@
 
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
+    //Forma Errada
+
+    axios.get(`${BASE_URL }/movies?size=12&page=0`)
+    .then(Response => {
+        console.log(Response.data);
+    });
     return (
 
         <>
