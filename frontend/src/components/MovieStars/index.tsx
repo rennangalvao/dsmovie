3bae9262b8a5cf58fac4008c065754d291de9429
Rennan Galvão <70344453+rennangalvao@users.noreply.github.com>
2022-01-21 16:21:38 -0500

    Update index.tsx
    Correção de erro no netlify

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -2,9 +2,6 @@ import {ReactComponent as StarFull } from 'assets/img/star-full.svg';
 import {ReactComponent as StarHalf } from 'assets/img/star-half.svg';
 import {ReactComponent as StarEmpty } from 'assets/img/star-empty.svg';
 import './styles.css';
-import { type } from 'os';
-import { isFunction } from 'util';
-
 
 type Props = {
   score: number;
